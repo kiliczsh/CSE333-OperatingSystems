4c8d9969b0d43791b1f3e29bf5a0235ca64e2d65
cumalidemir <32676101+cumalidemir@users.noreply.github.com>
2017-10-12 02:44:35 +0300

    Add files via upload

--- 150114048_150115048_Pro1/Q2/q2.sh
+++ 150114048_150115048_Pro1/Q2/q2.sh
@@ -1,7 +1,24 @@
-	if [ "$1" != "" ]; then #if command have argument
-   		#baseDir = "$1"
-		echo "Script location: ${baseDir}"
-	else #if command doesn't have argument
-		baseDir=$(dirname $0)
-		echo "Script location: ${baseDir}"
-	fi
+IS_NOT_DIRECTORY=85
+ARG1=$1
+
+
+if [[ -z "$1" ]]; then
+    
+	echo "$PWD using"
+	
+
+elif [[ -d $ARG1 ]]; then
+	echo "$ARG1 using"
+    directory="$1"
+	cd "$directory"
+
+else
+    echo "$ARG1 is not directory"
+    exit $IS_NOT_DIRECTORY
+fi
+
+
+shopt -s extglob
+	rm !(*.sh|*.c|"makefile"|"Makefile")
+
+
