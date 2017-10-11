85c9f9fb903d87e6419e3dbc37cc4fba58daf6fa
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2017-10-11 13:29:16 +0300

    Add files via upload

--- 150114048_150115048_Pro1/Q5/q5.sh
+++ 150114048_150115048_Pro1/Q5/q5.sh
@@ -6,9 +6,16 @@ firstIndex=0
 secondIndex=1
 #Extract the nuber of digits/characters
 len=`echo ${#number}`
+echo ""
 arrayOfDigits[len]=""
 arrayOfNumbers[len-1]=""
 index=0
+
+#if [len<=2 ] then
+#	echo "errorrr"
+#	exit 0
+#fi
+
 #Read the input contents digit by digit
 while [ $i -le $len ]
 do
@@ -16,17 +23,24 @@ do
     digit=`echo $number|cut -c$i`
     arrayOfDigits[index]=$digit
     echo "${arrayOfDigits[@]}"
-    let "index++"
+    index=`expr $index + 1`
 #Add it to the output variable
     output=`expr $output + $digit`
     i=`expr $i + 1`
 done
 k=0
-while (firstIndex<len-2 && secondIndex<len-1) do
-	arrayOfNumber[k] =(($arrayOfDigits[secondIndex]*10)+($arrayOfDigits[firstIndex]))
+while (firstIndex<len-1 && secondIndex<len-1 ) do
+	echo "$k"
+	echo "${arrayOfDigits[secondIndex]} is second index"
+	echo ""
+	echo "${arrayOfDigits[firstIndex]} is first index"
+	echo ""	
+#arrayOfNumber[k] =(($arrayOfDigits[secondIndex]*10)+($arrayOfDigits[firstIndex]))
 	k +=1
+	echo $index
+	echo ""
 done
-
+echo ""
 #Display the output 
 printf "\n\n"
 echo '********************************************'
