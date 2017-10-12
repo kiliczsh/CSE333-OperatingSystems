e2af49f8b8c3535ca0bb49ca4e6cc65803c7a956
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2017-10-12 19:19:38 +0300

    Add files via upload

--- 150114048_150115048_Pro1/menu/myProgram.sh
+++ 150114048_150115048_Pro1/menu/myProgram.sh
@@ -20,19 +20,21 @@ do
         "Print asterisks")
 		cd ..
 		cd Q1/
+		chmod +x *.*
 		./q1.sh "text1.txt"
 		;;
         "Delete files")
-		echo "you chose choice 2"
 		cd ..
                 cd Q2/
-                #./q2.sh
+                chmod +x *.*
+                ./q2.sh
                 ;;
 
         "Substitute words")
 		echo "you chose choice 3"
                 cd ..
                 cd Q3/
+                chmod +x *.*
 		printf "\nEnter a word to be replaced:\n"
 		read var1
 		printf "\nEnter a word to replace:\n"
@@ -42,17 +44,19 @@ do
 	"Organize directory")
 		cd ..
                 cd Q4/
+                chmod +x *.*
                 ./q4.sh
                 ;;
         "Print sum of numbers")
                 cd ..
                 cd Q5/
+                chmod +x *.*
 		printf "\nEnter a number to make calculations:\n"
 		read var
                 ./q5.sh $var
                 ;;
         "Exit")
-		seq 1 10000 | while read i; do echo -en "\r$i"; done
+		seq 1 15000 | while read i; do echo -en "\r$i"; done
 		echo -en "\rSee you next time..."
 		echo ""
             break
