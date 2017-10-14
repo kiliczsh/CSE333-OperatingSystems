3a02f42f0918f4909a378c09628fc4fdc7e60ecc
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2017-10-14 15:57:19 +0300

    Update q5.sh

--- 150114048_150115048_Pro1/Q5/q5.sh
+++ 150114048_150115048_Pro1/Q5/q5.sh
@@ -1,24 +1,28 @@
+#get argument
 number="$1"
-
- if [ $number -lt 10 ] 
- then
+#if the number is one digit case
+if [ $number -lt 10 ] 
+then
 	echo "$number=$number"
 	exit 1
- fi
-
+fi
+#
 len=`echo ${#number}`
 i=0
 arrayOfDigits=""
+#get the digits
 while [ $number -gt 0 ]
 do
 	arrayOfDigits[$i]=$(( $number % 10 ))
 	number=$(( $number / 10 ))
 	i=$((i+1))
 done
+
 first=1
 second=0
 limit=$(($len-1))
 sum=0
+#print all of the new numbers and print numbers
 while [ "$first" -lt "$len" ]
 do
 	if [ "$first" -lt "$limit" ]
@@ -34,5 +38,6 @@ do
         	second=$(($second + 1))
 	fi
 done
-	echo "=$sum"
+#print sum of numbers	
+echo "=$sum"
 
