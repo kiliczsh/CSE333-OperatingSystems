c8390c3a0c601bf3b01ff2afa5e120168ea9fd77
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2017-10-14 15:43:47 +0300

    Add files via upload

--- 150114048_150115048_Pro1/Q2/q2.sh
+++ 150114048_150115048_Pro1/Q2/q2.sh
@@ -3,30 +3,17 @@ ARG1=$1
 
 
 if [[ -z "$1" ]]; then
-    
+ 
 	echo "$PWD using"
-	
 
 elif [[ -d $ARG1 ]]; then
 	echo "$ARG1 using"
     directory="$1"
 	cd "$directory"
-	
 
 else
     echo "$ARG1 is not directory"
     exit $IS_NOT_DIRECTORY
 fi
-
-
-
-
-
 	shopt -s extglob
 	rm -f !(*.sh|*.c|"makefile"|"Makefile")
-
-
-
-
-
-
