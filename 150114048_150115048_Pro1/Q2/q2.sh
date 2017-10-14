fdd7507ae067569d9e28824d2b1ee5425aa5eb6d
cumalidemir <32676101+cumalidemir@users.noreply.github.com>
2017-10-15 02:35:04 +0300

    Update q2.sh

--- 150114048_150115048_Pro1/Q2/q2.sh
+++ 150114048_150115048_Pro1/Q2/q2.sh
@@ -1,19 +1,19 @@
-IS_NOT_DIRECTORY=85
-ARG1=$1
+IS_NOT_DIRECTORY=85#error code
+ARG1=$1#first argument
 
 
-if [[ -z "$1" ]]; then
+if [[ -z "$1" ]]; then#there is no input use current directory
  
-	echo "$PWD using"
+	echo "$PWD using"#echo info
 
-elif [[ -d $ARG1 ]]; then
+elif [[ -d $ARG1 ]]; then#if there is argument correctly use this
 	echo "$ARG1 using"
     directory="$1"
 	cd "$directory"
 
 else
-    echo "$ARG1 is not directory"
+    echo "$ARG1 is not directory"#show error and exit with error code
     exit $IS_NOT_DIRECTORY
 fi
 	shopt -s extglob
-	rm -f !(*.sh|*.c|"makefile"|"Makefile")
+	rm -f !(*.sh|*.c|"makefile"|"Makefile")#if there are files, remove all except that .c .sh makefile or Makefile. 
