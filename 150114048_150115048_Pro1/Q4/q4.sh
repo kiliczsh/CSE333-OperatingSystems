b6c9cd9fd4cc47a50e3b22a159d18db923530807
Muhammed Kılıç <muhammeddkilicc@users.noreply.github.com>
2017-10-14 15:54:57 +0300

    Update q4.sh

--- 150114048_150115048_Pro1/Q4/q4.sh
+++ 150114048_150115048_Pro1/Q4/q4.sh
@@ -1,15 +1,19 @@
 #ls -lh
-largestFile=`find . -maxdepth 1 -type f -exec basename {} \; | sort -n | head -1`
 
+#find the files just in the current directory and get the biggest file
+largestFile=`find . -maxdepth 1 -type f -exec basename {} \; | sort -n | head -1` 
+#find the files just in the current directory and get the smallest file
 smallestFile=`find . -maxdepth 1 -type f -exec basename {} \; | sort -n --reverse | head -1`
-
- BASEDIR=$(pwd)
- echo -en "\r"
- mkdir -p "$BASEDIR/"largest/
- mkdir -p "$BASEDIR/"smallest/
- mv  "$BASEDIR/$largestFile" "$BASEDIR/"largest/
- mv  "$BASEDIR/$smallestFile" "$BASEDIR/"smallest/
- echo "$largestFile is moved to the directory largest"
- echo "$smallestFile is moved to the directory smallest"
-
+#get the current directory
+BASEDIR=$(pwd)
+echo -en "\r"
+#check if the directories exist
+mkdir -p "$BASEDIR/"largest/
+mkdir -p "$BASEDIR/"smallest/
+#move the files to given directories
+mv  "$BASEDIR/$largestFile" "$BASEDIR/"largest/
+mv  "$BASEDIR/$smallestFile" "$BASEDIR/"smallest/
+#echo the movements
+echo "$largestFile is moved to the directory largest"
+echo "$smallestFile is moved to the directory smallest"
 #ls -lh
