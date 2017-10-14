90d18aeda014638715977258019101315becdcaa
cumalidemir <32676101+cumalidemir@users.noreply.github.com>
2017-10-15 02:37:21 +0300

    Update q3.sh

--- 150114048_150115048_Pro1/Q3/q3.sh
+++ 150114048_150115048_Pro1/Q3/q3.sh
@@ -1,26 +1,26 @@
-ARG1_MISSING_ERROR=55
-ARG2_MISSING_ERROR=56
-ARG3_MISSING_ERROR=57
-NOFILE=58
+ARG1_MISSING_ERROR=55#error code
+ARG2_MISSING_ERROR=56#error code
+ARG3_MISSING_ERROR=57#error code
+NOFILE=58#error code
 if [ -z "$1" ] 
   then
-    echo "3 arguments are missing"
+    echo "3 arguments are missing"#show error
 exit $ARG1_MISSING_ERROR
 fi
 
 if [ -z "$2" ]
   then
-    echo "2 arguments are missing"
-exit $ARG2_MISSING_ERROR
+    echo "2 arguments are missing"#show error
+exit $ARG2_MISSING_ERROR#exit with error code
 fi
 
 if [ -z "$3" ]
   then
-    echo "1 arguments are missing"
-exit $ARG1_MISSING_ERROR
+    echo "1 arguments are missing"#show erro
+exit $ARG1_MISSING_ERROR#exit with error code
 fi
 
-if [ ! -f "$1" ]       # Check if file exists.
+if [ ! -f "$1" ]       # Check  file exists.
 then
   echo "File \"$1\" does not exist."
   exit $E_NOFILE
@@ -29,7 +29,7 @@ fi
 
 sed -i -e "s/$2/$3/g" $1
 
- number_Of_Common=$(grep -o '\<'$3'\>' $1 | wc -l)
+ number_Of_Common=$(grep -o '\<'$3'\>' $1 | wc -l)#change word according to argument
 
 echo "All $number_Of_Common occurrences of '$2' in '$1' has changed with '$3' "
 
