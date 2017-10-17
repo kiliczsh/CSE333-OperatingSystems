d0c102bdba652dba46addb2554905ff624f35483
cumalidemir <32676101+cumalidemir@users.noreply.github.com>
2017-10-17 17:00:19 +0300

    Update q3.sh

--- 150114048_150115048_Pro1/Q3/q3.sh
+++ 150114048_150115048_Pro1/Q3/q3.sh
@@ -16,7 +16,7 @@ fi
 
 if [ -z "$3" ]
   then
-    echo "1 arguments are missing" #show erro
+    echo "1 argument are missing" #show erro
 exit $ARG1_MISSING_ERROR #exit with error code
 fi
 
@@ -31,5 +31,9 @@ sed -i -e "s/$2/$3/g" $1
 
  number_Of_Common=$(grep -o '\<'$3'\>' $1 | wc -l) #change word according to argument
 
+if [[ $number_Of_Common -eq 0 ]]
+then
+	echo "there is no '$2' in text file."
+exit
+fi
 echo "All $number_Of_Common occurrences of '$2' in '$1' has changed with '$3' "
-
